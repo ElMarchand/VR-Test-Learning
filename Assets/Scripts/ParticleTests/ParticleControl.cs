c9aedd11cb5757f508ef61fee64030632ac31ebd
ElMarchand <elricringuet@gmail.com>
2023-09-04 15:04:34 +0200

    First commit

---
+++ Assets/Scripts/ParticleTests/ParticleControl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*ParticleSystem part = GetComponent<ParticleSystem>();
+        var trails = part.trails;
+        trails.enabled = true;
+        trails.ratio = 0.5f;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
